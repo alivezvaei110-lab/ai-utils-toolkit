992385e9905f08ebb4f9b93f256bba326f668563
ali vezvaei <alivezvaei110@gmail.com>
2025-12-11 23:03:52 +0330

    Update text_cleaner.py

--- text_cleaner.py
+++ text_cleaner.py
@@ -14,3 +14,4 @@ if __name__ == "__main__":
     sample = "Hello!!!   This   is   a   test... 123!!!"
     print("Before:", sample)
     print("After:", clean_text(sample))
+# This is a simple text cleaner utility
